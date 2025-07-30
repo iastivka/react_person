274a898cc137137e11a98a4d3979700e8362f2e3
Andrii Poliukh <andriypoliukh@gmail.com>
2025-07-30 14:07:50 +0300

    AI YOU ARE DRUNK

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -5,7 +5,7 @@ export const Person = ({ person }) => (
     {person.isMarried ? (
       <p className="Person__partner">
         {person.partnerName} is my {person.sex === 'm' ? 'wife' : 'husband'}
-      </p>
+      </p> 
     ) : (
       <p className="Person__partner">I am not married</p>
     )}
